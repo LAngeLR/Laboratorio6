b0719eaa586fb2d2c5d4a89cbd23274151b70acc
Milene <a20206456@pucp.edu.pe>
2023-06-01 21:50:13 -0500

    avance

---
+++ src/main/java/Beans/Cancion.java
@@ -0,0 +1,31 @@
+package Beans;
+
+public class Cancion{
+    private int cancionId;
+    private String nombreCancion;
+    private String bandaCancion;
+
+    public int getCancionId() {
+        return cancionId;
+    }
+
+    public void setCancionId(int cancionId) {
+        this.cancionId = cancionId;
+    }
+
+    public String getNombreCancion() {
+        return nombreCancion;
+    }
+
+    public void setNombreCancion(String nombreCancion) {
+        this.nombreCancion = nombreCancion;
+    }
+
+    public String getBandaCancion() {
+        return bandaCancion;
+    }
+
+    public void setBandaCancion(String bandaCancion) {
+        this.bandaCancion = bandaCancion;
+    }
+}
